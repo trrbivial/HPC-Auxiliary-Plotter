5be9ad17bb2bb8d6e374bbc751639581ffcd2b4f
Jiajie Chen <c@jia.je>
2024-03-27 13:24:59 +0800

    Add initial project

---
+++ project-template-xilinx.srcs/sources_1/new/mod_top.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/27/2024 01:24:29 PM
+// Design Name: 
+// Module Name: mod_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mod_top(
+
+    );
+endmodule
